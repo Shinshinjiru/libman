8fd18751742ef32520447e6a59f8bf79e4bcfe1e
manulaiko <manulaiko@gmail.com>
2022-01-01 10:26:51 +0100

    Create library scanning and parsing

--- src/main/java/com/manulaiko/shinshijiru/libman/LibmanApplication.java
+++ src/main/java/com/manulaiko/shinshijiru/libman/LibmanApplication.java
@@ -1,13 +1,45 @@
 package com.manulaiko.shinshijiru.libman;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.nio.file.Path;
+
 @SpringBootApplication
-public class LibmanApplication {
+@Slf4j
+public class LibmanApplication implements ApplicationRunner {
+    public static void main(String[] args) {
+        SpringApplication.run(LibmanApplication.class, args);
+    }
 
-	public static void main(String[] args) {
-		SpringApplication.run(LibmanApplication.class, args);
-	}
+    @Autowired
+    private Manager manager;
 
+    /**
+     * Callback used to run the bean.
+     *
+     * @param args incoming application arguments
+     */
+    public void run(ApplicationArguments args) {
+        if (!args.containsOption("path")) {
+            log.error("Specify a path to the library with --path");
+
+            return;
+        }
+
+        log.info("Library path set to {}", args.getOptionValues("path"));
+
+        var libs = manager.scan(
+				args.getOptionValues("path")
+						.stream()
+						.map(Path::of)
+						.toList()
+		);
+
+        log.info("Result {}", libs);
+    }
 }
