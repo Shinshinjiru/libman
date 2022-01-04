6f367f6311612f760a81d17072d03a0b576be7e7
Manuel Sellers Rico <manuel.sellers.rico@everis.com>
2022-01-04 15:31:09 +0100

    Group shows by season

--- src/main/java/com/manulaiko/shinshijiru/libman/model/Library.java
+++ src/main/java/com/manulaiko/shinshijiru/libman/model/Library.java
@@ -19,25 +19,39 @@ public record Library(String name, List<Show> shows, String path) {
      * A show consists of:
      *  * A title for the show.
      *  * A flag to identify if the show is a series or a movie.
-     *  * A season number (not a counter, each season represents a different show).
-     *  * A list of episodes.
-     *  * A list of tags (Like fansub, resolution, audio...)
+     *  * A list of seasons.
+     *  * A list of tags (Like fansub, resolution, audio...).
      *  * A file path.
      *
      * @author manulaiko
      */
-    public record Show(String title, boolean movie, byte season, List<Episode> episodes, List<String> tags, String path) {
+    public record Show(String title, boolean movie, List<Season> seasons, List<String> tags, String path) {
         /**
-         * Represents a single episod.
+         * Represents a season of a show.
          *
-         * An episode consits of:
-         *  * An episode number (it's a string because of specials and recaps sometimes named as .5).
-         *  * An episode title.
-         *  * A list of tags (Like fansub, resolution, audio...)
-         *  * A file name.
+         * A season consists of:
+         *  * A season title.
+         *  * A season number.
+         *  * A list of episodes.
+         *  * A list of tags (Like fansub, resolution, audio...).
+         *  * A file path.
          *
          * @author manulaiko
          */
-        public record Episode(String number, String title, List<String> tags, String path) {}
+        public record Season(String title, byte season, List<Episode> episodes, List<String> tags, String path) {
+            /**
+             * Represents a single episode.
+             *
+             * An episode consists of:
+             *  * An episode number (it's a string because of specials and recaps sometimes named as .5).
+             *  * An episode title.
+             *  * A list of tags (Like fansub, resolution, audio...)
+             *  * A file name.
+             *
+             * @author manulaiko
+             */
+            public record Episode(String number, String title, List<String> tags, String path) {
+            }
+        }
     }
 }
